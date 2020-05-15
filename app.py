ff1e5849d2a802ce48978f271420976ddb9ae3ee
JinHuai Xuan <Jinhuai.Xuan38@myhunter.cuny.edu>
2020-05-15 03:05:20 -0400

    Implemented phase 2.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from pyspark import SparkContext
 
 def phaseOne(data, sc):
     print ("phase 1 ----------------------------------------------")
+
+    # map
     results = [] # list of pairs
     split = data.map(lambda x: x.split()).collect()
     for x in split:
@@ -11,12 +13,29 @@ def phaseOne(data, sc):
             results.append(pair)
     phaseOne = sc.parallelize(results)
     mapped = phaseOne.map(lambda x: (x, 1)) # ((document_id, term), 1)
+
+    # reduce
     occurrences = mapped.groupByKey().mapValues(len) # ((document_id, term), occurrences)
     return occurrences
 
-
 def phaseTwo(data, sc):
     print ("phase 2 ----------------------------------------------")
+    # map
+    mapped = data.map(lambda x: (x[0][0], (x[0][1], x[1]))) # (document_id, (term, occurrences))
+    mapped2 = mapped.groupByKey().mapValues(list).collect() # (document_id, [(term1,occurrences1), (term2, occurrences2)])
+
+    # reduce
+    results = []
+    for document in mapped2:
+        sum = 0
+        for term in document[1]:
+            sum += term[1]
+        for term in document[1]:
+            pair = ((document[0], term[0]), (term[1], sum))
+            results.append(pair)
+    reduced = sc.parallelize(results) # ((document_id, term), (term_occurrences, total_word_count_doc))
+    return reduced
+
 
 def phaseThree(data, sc):
     print ("phase 3 ----------------------------------------------")
@@ -27,7 +46,6 @@ def main():
     sc = SparkContext("local", "project")
     file = "project2_test.txt"
     data = sc.textFile(file)
-    total_num_of_documents = data.count()
 
     # first phase
     data_one = phaseOne(data, sc)
